b7734ed9dd246defe996101e542d7850cbdaaf67
Sébastien <sebastien.works@outlook.com>
2024-10-06 19:36:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_package',         # Name of your package
+    name='PolyGUIX',         # Name of your package
     version='0.1.0',             # Version number
-    description='A wrapper for Tkinter and CustomTkinter', 
+    description='A wrapper for Tkinter and CustomTkinter. Easier to use!', 
     long_description=open('README.md').read(), 
     long_description_content_type='text/markdown', 
-    url='https://github.com/yourusername/your_package',  # GitHub URL
+    url='https://github.com/striatp/PolyGUIX',  # GitHub URL
     author='Your Name', 
-    author_email='youremail@example.com', 
+    author_email='sebastien.works@outlook.com', 
     license='MIT', 
     packages=find_packages(),    # Automatically finds all packages in your_package/
     install_requires=[           # Dependencies required for your package
         'tkinter',
         'customtkinter',
+        'typing'
     ],
     classifiers=[                # Metadata for your package
         'Programming Language :: Python :: 3',
